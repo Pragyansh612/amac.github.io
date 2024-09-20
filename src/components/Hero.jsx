c5e53f35f0390a93cf4c4f27bc3a754dacb08784
Pragyansh612 <b23328@students.iitmandi.ac.in>
2024-09-20 16:44:24 +0530

    text hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { RobotCanvas } from './canvas';
 
 const Hero = () => {
-  <style>
-    {`
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <style>
+        {`
           @font-face {
             font-family: 'Carilliantine';
             src: url('./font/fonts/fonnts.com-Carilliantine_Solid.otf') format('opentype');
@@ -12,17 +14,40 @@ const Hero = () => {
           .custom-font {
             font-family: 'Carilliantine', sans-serif; /* Fallback to sans-serif */
           }
+
+          .fade-in {
+            opacity: 0;
+            animation: fadeIn 1.5s forwards;
+          }
+
+          @keyframes fadeIn {
+            to {
+              opacity: 1;
+            }
+          }
+
+          .slide-in {
+            transform: translateY(20px);
+            opacity: 0;
+            animation: slideIn 1.5s forwards;
+          }
+
+          @keyframes slideIn {
+            to {
+              transform: translateY(0);
+              opacity: 1;
+            }
+          }
+
         `}
-  </style>
-  return (
-    <section className="relative w-full h-screen mx-auto">
+      </style>
       <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
         <div className="text-white mx-20 text-xl">
-          <div className=' flex gap-3'>
-            <h1 className="text-4xl mt-2 poppins-semibold">Welcome to </h1>
-            <h1 className="text-5xl custom-font font-bold text-purple-800">Amac Stellar Studio</h1>
+          <div className="flex gap-3">
+            <h1 className={`text-5xl mt-3 poppins-semibold fade-in`}>Welcome to</h1>
+            <h1 className={` text-6xl custom-font font-bold text-purple-800 slide-in `}>Amac Stellar Studio</h1>
           </div>
-          <p className="mt-2 text-white-100 poppins-semibold">
+          <p className={`mt-2 text-2xl text-white-100 poppins-semibold fade-in`}>
             Where Creativity Meets Precision
           </p>
         </div>
